408166ad0bbd5c04e8dd8f0ce57e66c0b775f47a
shohan <31871498+shohanshohan@users.noreply.github.com>
2018-06-15 15:35:59 +0800

    Create 获取屏幕像素大小.js

---
+++ javascript/js,jQuery/获取屏幕像素大小.js
@@ -0,0 +1,17 @@
+window.screen 返回屏幕宽高的像素，和实际可用的大小
+console.log(window.screen); 
+// 信息如下
+availHeight:1040 // 实际可用高度
+availLeft:0
+availTop:0
+availWidth:1920 //实际可用宽度
+colorDepth:24
+height:1080  //高
+orientation:
+ScreenOrientation {angle: 0, type: "landscape-primary", onchange: null}
+pixelDepth:24
+width:1920 //宽
+
+
+
+查看是否浏览器是否连网状态：navigator.onLine    // 是的话返回true
