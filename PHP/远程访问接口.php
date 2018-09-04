cdcddb0510ad870b27d1851e516f5555c02a96a1
shohan <31871498+shohanshohan@users.noreply.github.com>
2018-09-04 11:00:05 +0800

    Update 远程访问接口.php

--- PHP/远程访问接口.php
+++ PHP/远程访问接口.php
@@ -6,10 +6,29 @@ function remoteHttp($url,$method='GET',$data=[])
     'http' => [
       'method' => $method,
       'header' => 'Content-type: application/x-www-form-urlencoded',
-      'content' => $data
+      'content' => http_build_query($data)
     ]
   ];
-  $context = steam_context_create($opts);
+  $context = stream_context_create($opts);
   return file_get_contents($url,false,$context);
 }
-比curl方法简便多了
+//比curl方法简便多了
+
+//如果要获取请求http头信息,可以如下：
+function remoteHttp($url,$method='GET',$data=[])
+{
+  $opts = [
+    'http' => [
+      'method' => $method,
+      'header' => 'Content-type: application/x-www-form-urlencoded',
+      'content' => http_build_query($data)
+    ]
+  ];
+  $context = stream_context_create($opts);
+  $fp = @fopen($url,'r');
+  $httpData = stream_get_meta_data($fp);
+  return [
+    'apiContent'  => file_get_contents($url,false,$context),
+    'httpInfo'    => $httpData['wrapper_data']
+  ]
+}
