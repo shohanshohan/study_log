46ae498e70c98a0f1322040e1513d2db2ad85f49
shohan <31871498+shohanshohan@users.noreply.github.com>
2019-08-29 14:56:19 +0800

    Update 百钱买百鸡.py

--- Python语言/应用/百钱买百鸡.py
+++ Python语言/应用/百钱买百鸡.py
@@ -11,12 +11,12 @@ price3 = 1 #鸡雏价格钱一（三只）
 #---------------
 # 思路：
 # 假设鸡翁的数量为 x, 鸡母的数量为 y 
-# 因为我们知道鸡的总数为 100 ,所以 x 或 y 的值范围 一定在 1~100 之间
+# 因为我们知道鸡的总数为 100 ,所以 x 或 y 的值范围 一定在 0~100 之间
 # 用两个循环算出所有鸡的总价钱等于 100 ，得出符合条件的组合
 #---------------
-for x in range(1, chicken+1):
+for x in range(0, chicken+1):
 	money1 = x * price1 # 买鸡翁花的钱
-	for y in range(1, chicken+1):
+	for y in range(0, chicken+1):
 		money2 = y * price2 # 买鸡母花的钱
 		money3 = (chicken-x-y) * price3 / 3 # 买鸡雏花的钱
 		if money1 + money2 + money3 == money:
@@ -27,3 +27,25 @@ for x in range(1, chicken+1):
 # 用上面方法还可以算出千钱买千鸡的组合，只需要把鸡和钱的数量改成1000
 # 价格不变，甚至可以求任意鸡总数 = 钱总数的组合了
 #-------------------
+
+
+#--------------------
+# 上面的方法还可以再改进一下
+# 我们取的鸡翁和鸡母的范围还可以缩小一点，这样性能更好
+# 因为我们知道钱的总数和价格，所以单个类别的鸡的数量乘以价格不能大于总钱
+# 所以鸡翁的数量范围是 range(0, 100//5) 即0~20，同理鸡母的数量范围是 0~33
+#--------------------
+for x in range(0, money // price1):
+	money1 = x * price1 # 买鸡翁花的钱
+	for y in range(0, money // price2):
+		money2 = y * price2 # 买鸡母花的钱
+		money3 = (chicken-x-y) * price3 / 3 # 买鸡雏花的钱
+		if money1 + money2 + money3 == money:
+			print('鸡翁：{}只，鸡母：{}只，鸡雏：{}只'.format(x, y, chicken-x-y))
+			
+			
+# 最精简的版本是下面这样，只要四行代码
+for x in range(0, money // price1):
+	for y in range(0, money // price2):
+		if x*price1 + y*price2 + (chicken-x-y)*price3/3 == money:
+			print('鸡翁：{}只，鸡母：{}只，鸡雏：{}只'.format(x, y, chicken-x-y))
