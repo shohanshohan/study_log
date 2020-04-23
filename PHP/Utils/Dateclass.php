be3f5c6ab666d48238ddd0340e49448ae9cd51ed
shohan <31871498+shohanshohan@users.noreply.github.com>
2020-04-23 15:49:51 +0800

    Update Dateclass.php

--- PHP/Utils/Dateclass.php
+++ PHP/Utils/Dateclass.php
@@ -10,13 +10,14 @@ class Dateclass
    * @param  [type]  $endDate   [结束日期]
    * @param  integer $offset    [分页起始点]
    * @param  integer $limit     [查询条数]
+   * @param  string  $timezone  [时区]
    * @param  string  $orderBy   [排序,升序asc,默认为倒序desc]
    * @param  [type]  $interval  [日期间隔/天,默认1天]
    * @return [type]             [Array]
    */
-  public static function getDates($startDate, $endDate, $offset=0, $limit=15, $orderBy='desc', $interval=1)
+  public static function getDates($startDate, $endDate, $offset=0, $limit=15, $timezone='Asia/Shanghai', $orderBy='desc', $interval=1)
   {
-    date_default_timezone_set('Asia/Shanghai');
+    date_default_timezone_set($timezone);
     if($orderBy === 'desc') { 
       //降序
       $firstDate = $offset==0 ? $endDate : date('Y-m-d', strtotime($endDate) - 86400*$offset);
