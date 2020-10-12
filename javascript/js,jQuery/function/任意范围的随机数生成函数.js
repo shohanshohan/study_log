8b7b67e5819a4a29e447a869fa582acd0d4f0a08
shohan <31871498+shohanshohan@users.noreply.github.com>
2020-10-12 10:18:51 +0800

    Update 任意范围的随机数生成函数.js

--- javascript/js,jQuery/function/任意范围的随机数生成函数.js
+++ javascript/js,jQuery/function/任意范围的随机数生成函数.js
@@ -1,4 +1,10 @@
 //取任意两个整数之间的随机数，不包含最大数，如果要生成 1-10（包含最小最大值则要传参数 1 和 11）
 function randNum(min, max) {
 	return Math.floor(Math.random() * (max - min) + min);
+	//如果不包含最小值则是  return Math.ceil(Math.random() * (max - min) + min);
+}
+
+//包含最小值 和 最大值的随机整数函数
+function rangeNum(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
 }
